5fc5290d48557ae183167439a98bfaf5df90ad7d
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-09-07 12:00:29 +0100

    Create Get started section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Landing from "../components/Landing"
 import Features from "../components/Features"
 import StayProductive from "../components/StayProductive"
 import Testimonials from "../components/Testimonials"
+import GetStarted from "../components/GetStarted"
 
 const Home = () => {
   return<Fragment>
@@ -10,6 +11,7 @@ const Home = () => {
       <Features/>
       <StayProductive/>
       <Testimonials/>
+      <GetStarted/>
   </Fragment>
 }
 
